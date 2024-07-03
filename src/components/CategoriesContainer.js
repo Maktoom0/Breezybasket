be4fbaf321400eeca97d694f2f490d5b7776c2e5
maktoom <ahmed.htbe@gmail.com>
2024-07-03 10:01:20 +0300

    fixed some images

--- src/components/CategoriesContainer.js
+++ src/components/CategoriesContainer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 import './style/categories-container.css'
 let categoriesImagesArray = [
-    {name: "chips", src: "https://raw.githubusercontent.com/Maktoom0/Breezybasket/main/public/categories-images/chips.webp"},
-    {name: "crisps", src: "https://raw.githubusercontent.com/Maktoom0/Breezybasket/main/public/categories-images/crisps.webp"},
+    {name: "chips", src: "https://raw.githubusercontent.com/Maktoom0/Breezybasket/main/public/categories-images/chips.png"},
+    {name: "crisps", src: "https://raw.githubusercontent.com/Maktoom0/Breezybasket/main/public/categories-images/crisps.jpg"},
     {name: "biscuits", src: "https://raw.githubusercontent.com/Maktoom0/Breezybasket/main/public/categories-images/biscuits.jpg"},
     {name: "juices", src: "https://raw.githubusercontent.com/Maktoom0/Breezybasket/main/public/categories-images/juices.png"}
 ]
@@ -12,8 +12,8 @@ function CategoriesContainer() {
     return (
         <div className='categories-container flex'>
             {categoriesImagesArray.map(element => 
-                <a href={`/trademark-search/none/${element.name}`} className='pointer'>
-                    <img className='full-width' src={element.src} alt={element.name} />
+                <a href={`/trademark-search/none/${element.name}`} className='pointer relative'>
+                    <img className='full-width absolute' src={element.src} alt={element.name} />
                     <p className='absolute'>{element.name}</p>
                 </a>
             )}
